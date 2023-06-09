07f08941016b949827f873315b2d619c4d52567f
tukki <tukki0210@gmail.com>
2023-06-09 16:46:28 +0900

    ナップサック

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"fmt"
+	"sort"
 )
 
 
@@ -20,7 +21,7 @@ func main() {
 	fmt.Println(odds)
 
 	// makeMatrix関数の動作確認
-	matrix := makeMatrix(3, 3)
+	matrix := makeMatrix(3, 4)
 	printMatrix(matrix)
 
 	// addMatrix関数の動作確認
@@ -33,6 +34,25 @@ func main() {
 	nums = []int{3, 2, 1, 4, 5}
 	fmt.Println(quickSort(nums))
 
+	// binarySearch関数の動作確認
+	nums = []int{1, 2, 3, 4, 5}
+	fmt.Println(binarySearch(nums, 3))
+	fmt.Println(binarySearch(nums, 6))
+
+	// fib関数の動作確認
+	fmt.Println(fib(10))
+
+	// knapsack関数の動作確認
+	weights := []int{2, 1, 3}
+	values := []int{3, 2, 4}
+	maxWeight := 5
+	fmt.Println(knapsack(weights, values, maxWeight))
+
+
+	// sort関数の動作確認
+	nums = []int{3, 2, 1, 4, 5}
+	sort.Ints(nums)
+	
 }
 
 // JavaScriptのmap関数
@@ -115,3 +135,95 @@ func quickSort(nums []int) []int {
 	}
 	return append(append(quickSort(left), pivot), quickSort(right)...)
 }
+
+// 二分探索
+func binarySearch(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) >> 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+// 動的計画法(フィボナッチ数列)
+func fib(n int) int {
+    dp := make([]int, n+1)
+    dp[0] = 0
+    dp[1] = 1
+    for i := 2; i <= n; i++ {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    return dp[n]
+}
+
+// 動的計画法(ナップサック問題)
+func knapsack(weights []int, values []int, maxWeight int) int {
+    n := len(weights)
+    dp := make([][]int, n+1)
+    for i := range dp {
+        dp[i] = make([]int, maxWeight+1)
+    }
+    
+    for i := 1; i <= n; i++ {
+        for w := 1; w <= maxWeight; w++ {
+            if weights[i-1] <= w {
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]]+values[i-1])
+            } else {
+                dp[i][w] = dp[i-1][w]
+            }
+        }
+    }
+    return dp[n][maxWeight]
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+// 深さ優先探索
+func dfs() {
+	// 隣接リスト
+	graph := [][]int{
+		{1, 2},
+		{0, 3, 4},
+		{0, 5, 6},
+		{1},
+		{1},
+		{2},
+		{2},
+	}
+	// 訪問済みの頂点
+	seen := make([]bool, len(graph))
+	// 深さ優先探索
+	var dfs func(v int)
+	
+	dfs = func(v int) {
+		seen[v] = true
+		for _, next := range graph[v] {
+			if seen[next] {
+				continue
+			}
+			dfs(next)
+		}
+	}
+	dfs(0)
+}
+
+// PairList型の定義
+type PairList [][]int
+
+// sort.Interfaceを実装
+func (p PairList) Len() int { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i][0] < p[j][0] } // 1つ目の要素を基に比較
+func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
